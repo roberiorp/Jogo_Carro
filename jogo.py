e20accd62e2cf5ee2bb35d562f267f95b7187865
root <root@DESKTOP-23S1KKG.localdomain>
2021-12-15 09:02:20 -0300

    Initial commit

---
+++ jogo.py
@@ -0,0 +1,69 @@
+from menu import * # pegando o arquivo Menu / o uso de "*" aparentemente é para incluir tudo
+
+
+class Game():
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("Laboratio de programação")
+        self.running, self.playing = True, False
+        self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
+        self.DISPLAY_W, self.DISPLAY_H = 720, 480 # Tamanho da tela
+        self.display = pygame.Surface((self.DISPLAY_W,self.DISPLAY_H))
+        self.window = pygame.display.set_mode(((self.DISPLAY_W,self.DISPLAY_H)))
+        self.font_name = 'fonte/PressStart2P-vaV7.ttf'
+        self.BLACK, self.WHITE = (0, 0, 0), (255, 255, 255) #Definindo cores
+        self.main_menu = MainMenu(self) #Classes
+        self.options = OptionsMenu(self)
+        self.credits = CreditsMenu(self)
+        self.curr_menu = self.main_menu
+
+    def game_loop(self):
+            while self.playing:
+                self.check_events()
+                if self.START_KEY:
+                    self.playing = False
+# INICIO
+                # # #  CONFIGURAÇÕES DE TELA ------------------------------------------------------------------
+                larguraTela, alturaTela = 1000, 500
+                metadeLargura = larguraTela / 2
+                metadeAltura = alturaTela / 2
+                areaTela = larguraTela * alturaTela
+                tela = pygame.display.set_mode((larguraTela, alturaTela))
+                bg = pygame.image.load("Cenario/cenario.gif")
+                background = pygame.transform.scale(bg, (larguraTela, alturaTela))
+                pygame.display.set_caption("Carro")
+
+                pygame.init()
+                CLOCK = pygame.time.Clock()
+                FPS = 45
+                BLACK = (0, 0, 0)
+
+                # # # MUSICA -----------------------------------------------------------------------------------
+                pygame.mixer.music.load('Musicas/start.wav')
+                pygame.mixer.music.play(-1)
+#FIM
+
+    def check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running, self.playing = False, False
+                self.curr_menu.run_display = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    self.START_KEY = True
+                if event.key == pygame.K_BACKSPACE or event.key == pygame.K_ESCAPE:
+                    self.BACK_KEY = True
+                if event.key == pygame.K_DOWN or event.key == pygame.K_s:
+                    self.DOWN_KEY = True
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
+                    self.UP_KEY = True
+
+    def reset_keys(self):
+        self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
+
+    def draw_text(self, text, size, x, y ): #Cor da letra, tamanho
+        font = pygame.font.Font(self.font_name,size)
+        text_surface = font.render(text, True, self.WHITE)
+        text_rect = text_surface.get_rect()
+        text_rect.center = (x,y)
+        self.display.blit(text_surface,text_rect)
---
+++ main.py
@@ -0,0 +1,9 @@
+from jogo import Game  # Pegando o arquivo game e importando a classe Game
+
+g = Game()  # facilitar toda a bagaça!
+
+while 1:
+    g.curr_menu.display_menu()
+    #g.game_loop()
+
+pygame.quit()
